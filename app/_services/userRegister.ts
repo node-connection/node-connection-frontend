4a8e6cb5cddce814076427dabec33622782a5931
Hyunbin Lee <blackhblee@gmail.com>
2024-09-22 10:59:27 +0900

    feat: add userRegister

---
+++ app/_services/userRegister.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import callApi from "../_utils/callApi";
+import getSessionToken from "../_utils/getSessionToken";
+
+type UserLoginProps = {
+  username: string;
+  email: string;
+  phoneNumber: string;
+  courtCode?: string;
+};
+
+const userRegister = async ({
+  username,
+  email,
+  phoneNumber,
+  courtCode,
+}: UserLoginProps) => {
+  const sessionToken = await getSessionToken();
+
+  const res = await callApi({
+    endpoint: "/user/register",
+    method: "POST",
+    token: sessionToken,
+    body: {
+      username,
+      email,
+      phoneNumber,
+      courtCode: courtCode || "",
+    },
+  });
+
+  console.log(res);
+
+  return res;
+};
+
+export default userRegister;
