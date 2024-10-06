81bd81798aca902dd49bf1f675e99b08c2012f2b
Hyunbin Lee <blackhblee@gmail.com>
2024-10-06 23:37:46 +0900

    feat: add RegistrationAddSection

---
+++ app/(route)/registration/_components/RegistrationEditSection.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const RegistrationEditSection = () => {
+  return (
+    <div>
+      <h1>Registration Edit Section</h1>
+    </div>
+  );
+};
+
+export default RegistrationEditSection;
