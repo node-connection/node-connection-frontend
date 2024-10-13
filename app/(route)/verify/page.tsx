256eabd977e4094824d0ea3ef236153c65c57ad3
Hyunbin Lee <blackhblee@gmail.com>
2024-10-14 02:08:21 +0900

    fix: fix typo

--- app/(route)/verify/page.tsx
+++ app/(route)/verify/page.tsx
@@ -8,9 +8,9 @@ const RegistrationPage = () => {
       <div className="flex flex-col items-center px-3 xl:px-0">
         <div className="flex w-full max-w-[1248px] flex-col items-center justify-center">
           <div className="flex w-full flex-col items-center border-b border-zinc-200 py-5">
-            <h1 className="text-2xl font-bold md:text-3xl">등기 검증</h1>
+            <h1 className="text-2xl font-bold md:text-3xl">등기 검증하기</h1>
             <h2 className="mt-3 text-base text-[#4E5968] md:text-lg">
-              발급 받은 부동산 등기사항증명서를 검증할 수 있습니다.
+              발급받은 부동산 등기사항증명서를 검증할 수 있습니다.
             </h2>
           </div>
           <VerifySection />
